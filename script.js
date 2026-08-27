2ae661b0a6de5e2f1ea3a2bddfb3963e8a311bcb
Henz00 <henzoo.hansen@gmail.com>
2026-08-27 22:07:35 +0200

    fixed menu brand transition

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("#header");
 
 window.addEventListener("scroll", () => {
-    header.classList.toggle("scrolled", window.scrollY > 0);
+    header.classList.toggle("scrolled", window.scrollY > 150);
 });
